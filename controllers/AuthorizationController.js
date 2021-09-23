010fe8b15298d794c1314a0468fbbcd834ada427
Kimhak <kolkimhak99@yahoo.com>
2021-09-23 22:55:29 +0700

    Update AuthorizationController.js

--- controllers/AuthorizationController.js
+++ controllers/AuthorizationController.js
@@ -8,17 +8,15 @@ import { hashPwd, validatePwd } from "../utils/permission";
 export async function login(req, res) {
     try {
         const user = await User.findOne({ username: req.body.username });
-        console.log(user, req.body);
-        console.log(await hashPwd("admin123"));
+        console.log(req.body);
 
         if (user) {
             const checkPwd = await validatePwd(req.body.password, user.password);
-            console.log(req.body.password);
-            console.log(user.password);
-            if(checkPwd) {
+
+            if (checkPwd) {
                 //u have whoever request their own info embeded inside token already, so no need to send anymore just reuse it
                 //u didnt put any role, should I?u can just pass id and then find  by id to get whatevr info u need ok
-                jwt.sign({ id: user._id, username: user.username, firstname: user.firstname, lastname: user.lastname, email: user.email }, process.env.TOKEN, {expiresIn: '10d'}, (err, token) => {
+                jwt.sign({ id: user._id, username: user.username, firstname: user.firstname, lastname: user.lastname, email: user.email }, process.env.TOKEN, { expiresIn: '10d' }, (err, token) => {
                     if (err) {
                         console.log("Log in Try Error ", err.message);
                         return res.status(200).json({ meta: meta.error.ERROR, message: err.message });
@@ -41,13 +39,13 @@ export async function login(req, res) {
     }
 }
 
-export async function register (req, res) {
+export async function register(req, res) {
     try {
         const { password, ...temp } = req.body;
         const user = new User({ ...temp, password: await hashPwd(password) });
         user.save()
             .then(data => {
-                res.status(200).json({meta: meta.normal.OK, message: msg.record.record_added})
+                res.status(200).json({ meta: meta.normal.OK, message: msg.record.record_added })
             })
             .catch(err => {
                 console.log("register Try Error ", err.message);
