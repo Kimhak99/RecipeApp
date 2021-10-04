49961a247b15e15004dedc9ce090b27cbc5d7c3d
ShadowGMR <teachhaylim12@gmail.com>
2021-10-04 18:57:16 +0700

    rework upload logic
    changes;:
    - separate upload setup
    - separate upload controller
    - others

--- controllers/UploadController.js
+++ controllers/UploadController.js
@@ -1,134 +1,37 @@
-import mongoose from "mongoose";
-import * as meta from "../utils/enum.js";
-import * as msg from "../utils/message.js";
+import { collectionName, gfs, gridFSBucket } from "../utils/upload";
 
-export async function upload(req, res) {
-  // req.file.md5 = req.file.id + req.file.md5;
-  console.log(req.file);
+const UploadFile = async (req, res) => {
+  res.status(201).send({ meta: 201, file: req.file });
+};
 
-  res.status(200).json({ file: req.file });
-}
+const GetFile = async (req, res) => {
+  let file = await gfs.files.findOne({ filename: req.params.filename });
 
-export async function uploads(req, res) {
-  const arr = req.files.map((p) => {
-    return {
-      md5: p.id + p.md5,
-      filename: p.filename,
-      contentType: p.contentType,
-      id: p.id,
-    };
-  });
+  if (!file) return res.status(404).send({ meta: 404, message: "File no exist" });
 
-  res.status(200).json({ meta: meta.normal.OK, files: arr });
-}
+  file = gridFSBucket.openDownloadStream(file._id);
+  res.writeHead(200, { 'Content-Type': 'image/png' });
+  file.pipe(res);
+};
 
-export async function deleteFile(req, res) {
-  try {
-    const id = mongoose.Types.ObjectId(req.params.md5.substring(0, 24));
-    const md5 = req.params.md5.substring(24, req.params.md5.length);
-    global.myGFS.files.findOne({ _id: id, md5: md5 }, (err, file) => {
-      if (err) {
-        res.status(400).json({ meta: meta.error.ERROR, err: msg.error_msg.ERROR });
-        return;
-      }
-      if (!file || file.length == 0) {
-        return res
-          .status(400)
-          .json({ meta: meta.error.NOTEXIST, message: msg.file_err_msg.notExist });
-      }
+const GetFileObject = async (req, res) => {
+  const file = await gfs.files.findOne({ filename: req.params.filename });
+  res.status(200).send({ meta: 200, file: file });
+};
 
-      global.myGFS.remove(
-        { _id: file._id, root: global.collectionName },
-        (err, gridStore) => {
-          if (err) {
-            return res
-              .status(400)
-              .json({ meta: meta.error.ERROR, err: msg.error_msg.notExist });
-          }
+const DeleteFile = async (req, res) => {
+  const file = await gfs.files.findOne({ filename: req.params.filename });
+  const err = await gfs.remove({ _id: file._id, root: collectionName });
 
-          res
-            .status(200)
-            .json({ meta: meta.normal.OK, err: msg.messages.record_delete });
-        }
-      );
-    });
-  } catch (error) {
-    console.log(req.params.md5);
-    res.status(500).json({
-      meta: meta.internal_error.ERROR,
-      message: error.message + " at deleteFile",
-    });
-  }
-}
+  if (!file) return res.status(404).send({ meta: 404, message: "File no exist" });
+  if (!err) return res.status(500).send({ meta: 500, message: "File delete error" });
 
-export async function deleteFileInternal(fileMd5) {
-  const id = mongoose.Types.ObjectId(fileMd5.substring(0, 24));
-  const md5 = fileMd5.substring(24, fileMd5.length);
+  return res.status(200).send({ meta: 200, file: file });
+};
 
-  const file = await global.myGFS.files.findOne({ _id: id, md5: md5 });
-
-  if (!file) {
-    console.log(file);
-    return meta.error.NOTEXIST;
-  }
-
-  await global.myGFS.remove({ _id: file._id, root: global.collectionName });
-
-  return meta.normal.OK;
-}
-
-export async function getFile(req, res) {
-  try {
-    // const id = mongoose.Types.ObjectId(req.params.md5.substring(0, 24));
-    // const md5 = req.params.md5.substring(24, req.params.md5.length);
-
-    global.myGFS.files.findOne({ filename: req.params.md5 }, (err, file) => {
-      console.log(file);
-
-      if (!file || file.length == 0) {
-        return res
-          .status(404)
-          .json({ meta: meta.error.NOTEXIST, message: msg.file_err_msg.notExist });
-      }
-
-
-      if (file.contentType === "application/pdf") {
-        const stream = global.myGFS.createReadStream(file.filename);
-        stream.pipe(res);
-      } else if (
-        file.contentType === "image/jpeg" ||
-        file.contentType === "image/png"
-      ) {
-        const stream = global.myGFS.createReadStream(file.filename);
-        stream.pipe(res);
-      } else {
-        res
-          .status(404)
-          .json({
-            meta: meta.NOTFILETYPE,
-            message: msg.file_err_msg.notFileType,
-          });
-      }
-    });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ meta: meta.internal_error.ERROR, message: "error" });
-  }
-
-}
-
-export async function getFileObj(req, res) {
-  const id = mongoose.Types.ObjectId(req.params.md5.substring(0, 24));
-  const md5 = req.params.md5.substring(24, req.params.md5.length);
-  global.myGFS.files.findOne({ _id: id, md5: md5 }, (err, file) => {
-    if (!file || file.length == 0) {
-      return res.status(404).json({
-        meta: meta.NOTEXIST,
-        err: msg.notExist,
-      });
-    }
-
-    return res.status(200).json({ meta: meta.OK, file: file });
-  });
+export default {
+  UploadFile,
+  GetFile,
+  GetFileObject,
+  DeleteFile,
 }
